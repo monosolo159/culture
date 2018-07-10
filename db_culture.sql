729c49b96a8689bb6dfe60ed50b80bc1d8c8755e
monosolo159 <wichetpong159@hotmail.com>
2018-07-10 13:39:18 +0700

    dd

--- db_culture.sql
+++ db_culture.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 05, 2018 at 05:41 PM
+-- Generation Time: Jul 10, 2018 at 08:38 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -46,10 +46,19 @@ INSERT INTO `department` (`department_id`, `department_name`) VALUES
 
 CREATE TABLE `media` (
   `media_id` int(11) NOT NULL,
-  `media_name` int(200) NOT NULL,
+  `media_name` varchar(200) NOT NULL,
+  `media_detail` varchar(500) NOT NULL,
   `media_link` varchar(200) NOT NULL,
   `media_type_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `media`
+--
+
+INSERT INTO `media` (`media_id`, `media_name`, `media_detail`, `media_link`, `media_type_id`) VALUES
+(1, 'rrrqqqq', 'GGGGHH', '36589918_1665798203488931_7833291573558247424_n.jpg', 1),
+(2, 'Doc', 'document', '001.docx', 2);
 
 -- --------------------------------------------------------
 
@@ -165,7 +174,7 @@ ALTER TABLE `department`
 -- AUTO_INCREMENT for table `media`
 --
 ALTER TABLE `media`
-  MODIFY `media_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `media_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `media_type`
 --
